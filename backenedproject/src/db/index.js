7d2685faac59339ee280bccd6fc8a4df42ab2264
Surajshivam-123 <sj919810@gmail.com>
2025-06-02 13:14:25 +0000

    backend setup

--- backenedproject/src/db/index.js
+++ backenedproject/src/db/index.js
@@ -4,11 +4,11 @@ import { DB_NAME } from "../constants.js"
 export const connectDB = async () =>{
     try {
         const connectionInstance= await mongoose.connect(`${process.env.MongoDB_URI}/${process.env.DB_NAME}`);
-        console.log(connectionInstance.Connection.host);
+        console.log(connectionInstance.connection.host);
         console.log("Connected to MongoDB");
 
     } catch (error) {
         console.log("MongoDB connection Error",error);
-        process.exit();
+        process.exit(1);
     }
 }
